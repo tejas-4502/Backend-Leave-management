2d16ae30ca44238e6909654d15bb9495c0954843
Tejas <tejassonawane171@gmail.com>
2023-12-14 11:30:58 +0530

    removed unneccesory

---
+++ Backend Leave/Migrations/20231214052401_rm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend_Leave.Migrations
+{
+    public partial class rm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
